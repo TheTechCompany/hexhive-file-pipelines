87d6745f38652b5c417e7183b4968f93d0a2cff4
Ross Leitch <professional.balbatross@gmail.com>
2020-12-07 13:10:41 +1300

    Wait for it

--- step2glb/worker.py
+++ step2glb/worker.py
@@ -5,18 +5,22 @@ credentals = pika.PlainCredentials('rabbitmq', 'rabbitmq')
 parameters = pika.ConnectionParameters('rabbit1', 5672, '/', credentals)
 
 def on_connected(connection):
+    print("Connected to rabbitmq")
     connection.channel(on_open_callback=on_channel_open)
 
 def on_channel_open(new_channel):
+    print("Channel opened")
     global channel
     channel = new_channel
     consume()
 #    channel.queue_declare(queue="stp2glb", durable=T
 
 def consume():
+    print("Consuming")
     channel.basic_consume("stp2glb", handle_delivery)
 
 def handle_delivery(channel, method, header, body):
+    print("STUFF")
     print(body)
 
 connection = pika.SelectConnection(parameters, on_open_callback=on_connected)
