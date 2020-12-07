3b952b0fda5c82fca12551d3f1a228d716483341
Ross Leitch <professional.balbatross@gmail.com>
2020-12-07 15:34:45 +1300

    Mongo?

--- gltfpack/worker.py
+++ gltfpack/worker.py
@@ -1,14 +1,21 @@
 import pika
 import subprocess
 import os
+import pymongo
 
 channel = None
 credentals = pika.PlainCredentials('rabbitmq', 'rabbitmq')
 parameters = pika.ConnectionParameters('rabbit1', 5672, '/', credentals)
 
+mongoClient = pymongo.MongoClient('mongodb://mongo:27017')
+myDb = mongoClient['cae']
+myCol = myDb['models']
+
 def handle_delivery(channel, method, header, body):
     print(body)
     input_ref = body.decode('utf-8')
+
+    myCol.update_one({"fileId": input_ref}, {"$set": {"status": "PREPACK"}})
 
     os.chdir("/data/cae/glb2glb")
     process_result = subprocess.run(args=['/usr/local/bin/gltfpack', '-i',
@@ -16,9 +23,11 @@ def handle_delivery(channel, method, header, body):
     '../gltfpack/'+ input_ref + '.glb'], env={"Model": input_ref})
 
     if(process_result.returncode == 0):
+        myCol.update_one({"fileId": input_ref}, {"$set": {"status": "PACKED"}})
         print("Success moving to gltfpack not full success but enough to get us moving")
         channel.basic_ack(delivery_tag=method.delivery_tag)
     else:
+        myCol.update_one({"fileId": input_ref}, {"$set": {"status": "GLB2GLB"}})
         print("Failure returning it to the queue")
         channel.basic_nack(delivery_tag=method.delivery_tag)
 
--- step2glb/worker.py
+++ step2glb/worker.py
@@ -1,22 +1,31 @@
 import pika
 import subprocess
+import pymongo
 
 channel = None
 credentals = pika.PlainCredentials('rabbitmq', 'rabbitmq')
 parameters = pika.ConnectionParameters('rabbit1', 5672, '/', credentals)
 
+mongoClient = pymongo.MongoClient('mongodb://mongo:27017')
+myDb = mongoClient['cae']
+myCol = myDb['models']
+
 def handle_delivery(channel, method, header, body):
-    print(body)
     input_ref = body.decode('utf-8')
+    
+    myCol.update_one({"fileId": input_ref}, {"$set": {"status": "PRE-STP2GLB"}})
+
     process_result = subprocess.run(['./step_to_gltf', '-o',
         '/data/cae/stp2glb/' + input_ref, '/data/cae/inputs/' + input_ref])
 
     if(process_result.returncode == 0):
         print("Success moving to glb2glb")
+        myCol.update_one({"fileId": input_ref}, {"$set": {"status": "STP2GLB"}})
         channel.basic_publish(exchange='', routing_key='glb2glb', body=body)
         channel.basic_ack(delivery_tag=method.delivery_tag)
     else:
         print("Failure returning it to the queue")
+        myCol.update_one({"fileId": input_ref}, {"$set": {"status": "PREPROCESS"}})
         channel.basic_nack(delivery_tag=method.delivery_tag)
 
 connection = pika.BlockingConnection(parameters)
