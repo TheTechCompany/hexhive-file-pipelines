a51468f4bf3479965998d1e21d82582475154112
Ross Leitch <professional.balbatross@gmail.com>
2020-12-07 13:51:06 +1300

    worker update

--- step2glb/worker.py
+++ step2glb/worker.py
@@ -8,9 +8,7 @@ parameters = pika.ConnectionParameters('rabbit1', 5672, '/', credentals)
 def handle_delivery(channel, method, header, body):
     print(body)
     input_ref = body.decode('utf-8')
-    process_result = subprocess.run(['./step_to_gltf', '-o',
-        '/data/cae/stp2glb/' + input_ref, '/data/cae/inputs/' + input_ref],
-        capture_output=True)
+    process_result = subprocess.run(['./step_to_gltf', '-o', '/data/cae/stp2glb/' + input_ref, '/data/cae/inputs/' + input_ref], capture_output=True)
 
     if(process_result.returncode == 0):
         channel.basic_ack(delivery_tag=method.delivery_tag)
