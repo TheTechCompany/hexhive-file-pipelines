d66242ffe2626a5a13cc5a46231ccb309a06e3bf
Ross Leitch <professional.balbatross@gmail.com>
2020-12-07 13:47:23 +1300

    worker update

--- step2glb/worker.py
+++ step2glb/worker.py
@@ -1,32 +1,29 @@
 import pika
+import subprocess
 
 channel = None
 credentals = pika.PlainCredentials('rabbitmq', 'rabbitmq')
-parameters = pika.ConnectionParameters('rabbit1', 5672, '/', credentals)
-
-def on_connected(connection):
-    print("Connected to rabbitmq")
-    connection.channel(on_open_callback=on_channel_open)
-
-def on_channel_open(new_channel):
-    print("Channel opened")
-    global channel
-    channel = new_channel
-    consume()
-#    channel.queue_declare(queue="stp2glb", durable=T
-
-def consume():
-    print("Consuming")
-    channel.basic_consume("stp2glb", handle_delivery)
+parameters = pika.ConnectionParameters('localhost', 5672, '/', credentals)
 
 def handle_delivery(channel, method, header, body):
-    print("STUFF")
     print(body)
+    input_ref = body.decode('utf-8')
+    process_result = subprocess.run(['./step_to_gltf', '-o',
+        '/data/cae/stp2glb/' + input_ref, '/data/cae/inputs/' + input_ref],
+        capture_output=True)
 
-connection = pika.SelectConnection(parameters, on_open_callback=on_connected)
+    if(process_result.returncode == 0):
+        channel.basic_ack(delivery_tag=method.delivery_tag)
+    else:
+        channel.basic_nack(delivery_tag=method.delivery_tag)
 
-try:
-    connection.ioloop.start()
-except KeyboardInterrupt:
-    connection.close()
-    connection.ioloop.start()
+connection = pika.BlockingConnection(parameters)
+
+channel = connection.channel()
+
+channel.queue_declare(queue='stp2glb', durable=True)
+
+channel.basic_consume(queue='stp2glb', auto_ack=False,
+        on_message_callback=handle_delivery)
+
+channel.start_consuming()
