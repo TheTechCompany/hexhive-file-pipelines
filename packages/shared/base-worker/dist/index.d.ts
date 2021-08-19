afaf9c7bf606250eed7d264ec897a5900afdc607
Ross Leitch <professional.balbatross@gmail.com>
2021-08-19 16:20:08 +1200

    updating stp2glb

--- packages/shared/base-worker/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const HiveWorker: (worker_fn: (opts: {
-    file: string;
-    jobId: string;
-    pipelineDirectory: string;
-}) => Promise<boolean>) => Promise<boolean>;
--- packages/shared/base-worker/dist/ipfs.d.ts
+++
@@ -1,8 +0,0 @@
-import IPFS from 'ipfs-http-client';
-declare const _default: (options: {
-    url: string;
-}) => Promise<{
-    addFile: (file: File) => Promise<IPFS.CID>;
-    getFile: (cid: string, tmpPath: string) => Promise<void>;
-}>;
-export default _default;
